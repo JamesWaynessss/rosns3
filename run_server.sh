07115fca2ca9dfb62961aa6b19d5d6706c9d4730
谢宇 <xieyu@xieyudeMacBook-Air.local>
2024-07-13 13:22:03 +0800

    Add new feature

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,7 @@
 # Enable debug logs for ROSNS3 Server
 ENABLE_LOGGING=0
 
-# Friis loss model parameters
-path_loss_exponent=2.4
-reference_loss=46;
-transmission_power=16.0206;
-fading_mean=0;
-fading_var=32;
+new test
 
 # WAF command to run rosns3 server
 # CMD_WAF="./waf --run 'rosns3-example --exp=$path_loss_exponent --ref_loss=$reference_loss --tx_power=$transmission_power --mean=$fading_mean --var=$fading_var'"
